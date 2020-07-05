b1c4cc551b00ba887ce044537c2c67499d847bf5
ingridmiranda <ingridmsousa@hotmail.com>
2020-07-05 12:14:13 -0300

    Inserir opcao de sexo

--- app/src/main/java/com/example/healthtracking/DadosPessoaisActivity.java
+++ app/src/main/java/com/example/healthtracking/DadosPessoaisActivity.java
@@ -2,17 +2,27 @@ package com.example.healthtracking;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.rtoshiro.util.format.MaskFormatter;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.pattern.MaskPattern;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class DadosPessoaisActivity extends Activity {
@@ -32,6 +42,71 @@ public class DadosPessoaisActivity extends Activity {
         final EditText txtPeso = findViewById(R.id.txtPeso);
         final EditText txtPhone = findViewById(R.id.txtPhone);
         final Button btSalvarCadastro = findViewById(R.id.btSaveCadastro);
+
+        final Spinner spinnerSexo = findViewById(R.id.spinnerSexo);
+
+        final String[] array_sexo = new String[]{"Sexo", "Feminino", "Masculino"};
+        final List<String> sexoList = new ArrayList<>(Arrays.asList(array_sexo));
+
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this,R.layout.spinner_item,sexoList){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spinnerSexo.setAdapter(spinnerArrayAdapter);
+
+        spinnerSexo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+/*
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sexo_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSexo.setAdapter(adapter);
+*/
 
         /**
         * Criação de máscara para data de nascimento
@@ -76,9 +151,17 @@ public class DadosPessoaisActivity extends Activity {
             @Override
             public void onClick(View v) {
                 dadosPessoais.setNome(txtName.getText().toString());
-                String name = dadosPessoais.getNome();
+                dadosPessoais.setDataNascimento(txtDataNascimento.getText().toString());
+                dadosPessoais.setEmail(txtEmail.getText().toString());
+                dadosPessoais.setCidade(txtCidade.getText().toString());
+                dadosPessoais.setTelefone(txtPhone.getText().toString());
 
+                dadosPessoais.setSexo(spinnerSexo.getSelectedItem().toString());
+
+                String name = dadosPessoais.getNome();
                 Log.d(TAG,  "nome: " + name);
+                Log.d(TAG, dadosPessoais.getCidade() + dadosPessoais.getDataNascimento() + dadosPessoais.getEmail() + dadosPessoais.getTelefone()
+                        + dadosPessoais.getSexo());
                 if (isCampoVazio(txtName)){
                     Toast.makeText(getApplicationContext(), "Campo obrigatório!", Toast.LENGTH_LONG).show();
                 } else {
